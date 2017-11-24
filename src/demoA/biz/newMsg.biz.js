24b23ab3479e6c66df9c34dd68baf10121810687
YSHI <1179082420@QQ.COM>
2017-11-24 11:11:55 +0800

    运行中自动修改文件
    替换msg.biz.js，运行到write()方法会修改friend.biz.js，热加载friend.biz.js，运行新内容。
    实际上没有if (a == 0) {}限制也不会热加载，也许是因为第二次及以后的修改不会是代码发生变化。

---
+++ src/demoA/biz/newMsg.biz.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+module.exports = {
+    evt: 'msg',
+    fn() {
+        console.log('msg hanlder....');
+        if (a == 0) {
+            write("friend.biz.js", `
+module.exports =  {
+evt:'friend', 
+fn() {
+console.log('friend hanlder....123'); 
+}
+}; 
+`);
+        }
+    }
+};
+var a = 0;
+
+function write(fileName, data) { //生成文件，写入内容
+    fs.open("E:/js/webpack-hot-nodejs-demo-master/src/demoA/biz/" + fileName, "w", 0644, function(e, fd) { //"a":没有则生成，否则追加
+        if (e) throw e;
+        fs.write(fd, data, 0, 'utf8', function(e) {
+            if (e) throw e;
+            a = 1;
+            fs.closeSync(fd);
+        });
+    });
+}
